0c52ecd4b2a7338bcfb97fc77ab939292eb569c9
Pablo Pareja Tobes <ppareja@era7.com>
2011-06-09 12:19:02 +0200

    first draft of Export5ColumnsGenBankFiles program

--- src/com/era7/bioinfo/annotation/gb/Export5ColumnsGenBankFiles.java
+++ src/com/era7/bioinfo/annotation/gb/Export5ColumnsGenBankFiles.java
@@ -5,6 +5,10 @@
 package com.era7.bioinfo.annotation.gb;
 
 import com.era7.lib.bioinfo.bioinfoutil.Executable;
+import com.era7.lib.bioinfoxml.Annotation;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 /**
@@ -23,15 +27,39 @@ public class Export5ColumnsGenBankFiles implements Executable{
 
     public static void main(String[] args) {
 
-        if (args.length != 4) {
-            System.out.println("This program expects 4 parameters: \n"
+        if (args.length != 3) {
+            System.out.println("This program expects 3 parameters: \n"
                     + "1. Gene annotation XML result filename \n"
-                    + "2. Contig external general info XML filename\n"
-                    + "3. FNA file with both header and contig sequence\n"
-                    + "4. Prefix string for output files\n");
+                    + "2. GenomeProject ID\n"
+                    + "3. Locus tag prefix\n") ;
         } else {
             
+            String annotationFileSt = args[0];
+            String genomeProjectIdSt = args[1];
+            String locusTagPrefixSt = args[2];
             
+            
+            File annotationFile = new File(annotationFileSt);
+            
+            try {
+
+                //-----READING XML FILE WITH ANNOTATION DATA------------
+                BufferedReader reader = new BufferedReader(new FileReader(annotationFile));
+                String tempSt;
+                StringBuilder stBuilder = new StringBuilder();
+                while ((tempSt = reader.readLine()) != null) {
+                    stBuilder.append(tempSt);
+                }
+                //Closing file
+                reader.close();
+
+                Annotation annotation = new Annotation(stBuilder.toString());
+                //-------------------------------------------------------------
+                
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             
         }
     }
